f4839bae78f172971caa27b38655c1e1d70f4309
Kyle King <KyleKing@users.noreply.github.com>
2023-02-06 19:14:20 -0500

    test: refacotr result variable name

--- tests/test_mdformat.py
+++ tests/test_mdformat.py
@@ -7,10 +7,10 @@ def test_mdformat_text():
     """Verify that using mdformat works as expected."""
     content = (Path(__file__).parent / "pre-commit-test.md").read_text()
 
-    out = mdformat.text(
+    result = mdformat.text(
         content,
         options={"end-of-line": "keep"},
         extensions={"admonition"},
     )
 
-    assert out == content
+    assert result == content
